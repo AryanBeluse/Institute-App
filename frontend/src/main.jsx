6622fd202559e1c676c22646400dfd63544d5c5e
AryanBeluse <aryan.beluse@gmail.com>
2025-04-30 16:20:43 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AdminContextProvider from './context/adminContext.jsx'
+import AdminContextProvider from './context/AdminContext.jsx'
 import TrainerContextProvider from './context/TrainerContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
